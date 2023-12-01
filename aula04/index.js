b338c913f88c5b7442aec4defbe7251db83a6c6e
debora <141441781+deboraaa22@users.noreply.github.com>
2023-12-01 16:13:32 -0300

    projeto2

--- aula04/index.js
+++ aula04/index.js
@@ -1,42 +1,74 @@
 const express = require('express')
 const app = express()
-const exphbs = require('express-handlebars')
 const port = 3000
+const exphbs = require('express-handlebars')
 
-//configure template hand
+//configure template handlebars
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 //parser para leitura do body
 app.use(
-    express.urlencoded({
-        extended: true
-    })
+express.urlencoded({
+extended: true
+})
 )
 app.use(express.json())
 
-//adcionando css
-app.use(express.static('public'))
+//ADICIONANDO CAMINHO CSS
+//app.use(express.static('public'))
 
 app.get('/users/add', (req, res) => {
-    res.render('userform')
-    })
-    
-    app.post('/users/save', (req, res) => {
-    const name = req.body.name;
-    const dataNasc = req.body.dataNasc;
-    const email = req.body.email;
-    const func = req.body.func;
-    const user = { name: name, dataNasc: dataNasc, email: email, func:func }
-    res.render('viewuser', { user: user })
-    
-    })
+res.render('userform')
+})
+
+app.post('/users/save', (req, res) => {
+const name = req.body.name
+const age = req.body.age
+const user = { name: name, age: age }
+res.render('viewuser', { user: user })
+
+})
+
+const usuario = {
+login: 'teste',
+senha: 123
+
+}
+
 
 app.get('/', (req, res) => {
-    res.render('home')
+
+res.render('login')
 })
 
+var auth = false
+
+app.post('/user/login', (req, res) => {
+const login = req.body.login
+const senha = req.body.senha
+let message = ""
+
+if (login == usuario.login && senha == usuario.senha) {
+auth = true
+message = "Usuário logado com sucesso!"
+res.render('home', { usuario: usuario, auth, message })
+}
+else {
+auth = false
+message = "Usuário e/ou senha inválidos!"
+res.render('login', { auth, message })
+}
+
+})
+
+//pagina 404
+app.use(function (req, res, next) {
+res.status(404).render('404')
+})
+
+
+//webserver
 app.listen(port, () => {
-    console.log('Server Started')
+console.log('Server Started')
 })
-
